c07071ff84e0a0daa3ae7308abee2d29c8a2cbc3
abishekchandramahato <abishek.mahato@metropolia.fi>
2019-08-05 22:42:47 +0300

    first commit for germany branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("abcd");
         System.out.println("Hello change");
+        System.out.println("Branch germany chan ges");
     }
 }
